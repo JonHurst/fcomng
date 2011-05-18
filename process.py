8ea90f565e7d48f208d23119f478125e3bbb9140
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2011-05-18 10:24:23 +0100

    Simplified code for better maintainability

--- process.py
+++ process.py
@@ -10,102 +10,130 @@ output_dir = "html/"
 data_dir= "fcom/DATA/"
 xsl_dir = "xsl/"
 
-class ProcessList:
+class FCOMMeta:
+
+
+    class Section:
+        def __init__(self, title):
+            self.title = title
+            self.children = []
+            self.du_list = []
+
+
+        def add_child(self, sid):
+            self.children.append(sid)
+
+
+        def add_du(self, du_filename):
+            self.du_list.append(du_filename)
+
 
     def __init__(self, rootelement):
-        self.__dict__ = {}
+        self.sections = {}
+        self.du_meta = {}
+        self.top_level_sids = []
         for psl in rootelement.findall("psl"):
-            # if psl.attrib["pslcode"] in ("FCB", "GEN"):
+            self.top_level_sids.append((psl.attrib["pslcode"],))
             self.__process_psl__(psl, ())
-        self.orderedkeys = self.__dict__.keys()
-        self.orderedkeys.sort()
 
 
     def __process_psl__(self, elem, sec_id):
         i = sec_id + (elem.attrib["pslcode"],)
-        self.__dict__[i] = [[], ]
-        pl = [elem.findtext("title"),]
+        self.sections[i] = []
+        section = self.Section(elem.findtext("title"))
+        self.sections[i] = section
+        if self.sections.has_key(i[:-1]):
+            self.sections[i[:-1]].add_child(i)
         for e in elem:
             if e.tag == "du-inv":
-                pl.append(self.__process_duinv__(e))
+                self.__process_duinv__(e, section)
             elif e.tag == "group":
-                pl += self.__process_group__(e)
+                self.__process_group__(e, section)
             elif e.tag == "psl":
                 self.__process_psl__(e, i)
-        self.__dict__[i].append(pl)
-        #add this psl to parent's list of children
-        if self.__dict__.has_key(i[:-1]):
-            self.__dict__[i[:-1]][0].append(i)
 
 
-    def __process_duinv__(self, elem):
+    def __process_duinv__(self, elem, section):
         retval = []
+        data_files = []
         for s in elem.findall("du-sol"):
-            retval.append((s.findtext("title"),
-                           s.find("sol-content-ref").attrib["href"],
-                           s.find("sol-mdata-ref").attrib["href"]))
-        return tuple(retval)
+            data_file = s.find("sol-content-ref").attrib["href"]
+            data_files.append(data_file)
+            section.add_du(tuple(data_files))
+            self.du_meta[data_file] = s.find("sol-mdata-ref").attrib["href"]
 
 
-    def __process_group__(self, elem):
+    def __process_group__(self, elem, section):
         #note: groups don't nest, and they only contain du-inv sections
-        pl = []
         for s in elem.findall("du-inv"):
-            pl.append(self.__process_duinv__(s))
-        return tuple(pl)
-
-
-    def level_list(self, level):
-        retval = []
-        for k in self.orderedkeys:
-            if len(k) == level or (
-                len(k) < level and len(self.__dict__[k][0]) == 0):
-                retval.append(k)
-        return retval
+            self.__process_duinv__(s, section)
 
 
     def get_title(self, sid):
-        return self.__dict__[sid][1][0]
+        return self.sections[sid].title
 
 
     def get_dus(self, sid):
-        return [X[0][1] for X in self.__dict__[sid][1][1:]]
-
-
-    def __offspring__(self, sid):
-        offspring = []
-        for o in self.__dict__[sid][0]:
-            offspring.append(o)
-            offspring += self.__offspring__(o)
-        return offspring
-
-
-    def dump(self):
-        keys = self.__dict__.keys()
-        keys.sort()
-        for k in keys:
-            print k
-            for p in self.__dict__[k]:
-                print "   ", p
-
-
-    def get_sids(self):
-        return self.orderedkeys[:]
+        return self.sections[sid].du_list
 
 
     def get_children(self, sid):
-        return self.__dict__[sid][0][:]
+        return self.sections[sid].children
 
 
-class PageFactory:
+    def get_all_sids(self):
+        retval = []
+        for s in self.top_level_sids:
+            retval.append(s)
+            retval += self.get_descendants(s)
+        return retval
 
-    def __init__(self, pl):
-        self.pl = pl
+
+    def get_descendants(self, sid):
+        retval = []
+        for c in self.get_children(sid):
+            retval.append(c)
+            retval += self.get_descendants(c)
+        return retval
+
+
+    def get_leaves(self, prune=3):
+        retval = []
+        for s in self.get_all_sids():
+            if len(s) > prune: continue
+            children = self.get_children(s)
+            if not children:
+                retval.append(s)
+        return retval
+
+
+    def dump(self):
+        print "Sections:\n==========\n"
+        for s in self.get_all_sids():
+            section = self.sections[s]
+            indent = " " * ((len(s) - 1) * 4)
+            print indent, ".".join(s), ": ", section.title
+            for du in self.get_dus(s):
+                print indent, " ", du
+        print "\n\nLeaves:\n=======\n"
+        for l in self.get_leaves():
+            print ".".join(l), ": ", self.get_title(l)
+        print "\n\nMeta:\n=====\n"
+        du_keys = self.du_meta.keys()
+        du_keys.sort()
+        for k in du_keys:
+            print k, self.du_meta[k]
+
+
+class FCOMFactory:
+
+    def __init__(self, fcm):
+        self.fcm = fcm #instance of FCOMMeta
 
 
     def build_fcom(self):
-        self.pagelist = self.pl.level_list(3)
-        self.make_index(self.pl)
+        self.pagelist = self.fcm.get_leaves()
+        # self.make_index(self.pl)
         for c, sid in enumerate(self.pagelist):
             prevsid, nextsid = None, None
             if c > 0:
@@ -116,58 +144,27 @@ class PageFactory:
 
 
     def __build_sid_list__(self, sid):
-        retval = [sid, ]
-        for s in self.pl.get_children(sid):
-            retval += self.__build_sid_list__(s)
-        return retval
+        return [sid, ] + self.fcm.get_descendants(sid)
 
 
     def make_page(self, c, sid, prevsid, nextsid):
         filename = self.__make_filename__(sid)
         print "Creating:", filename
-        root_element = et.Element("page", {"title": self.pl.get_title(sid[:1])})
+        root_element = et.Element("page", {"title": self.fcm.get_title(sid[:1])})
         if prevsid: root_element.set("prev", ".".join(prevsid) + ".html")
         if nextsid: root_element.set("next", ".".join(nextsid) + ".html")
-        if len(sid) > 1: root_element.set("subtitle", self.pl.get_title(sid[:2]))
+        if len(sid) > 1: root_element.set("subtitle", self.fcm.get_title(sid[:2]))
         for s in self.__build_sid_list__(sid):
             current = et.SubElement(root_element, "section", {"sid": ".".join(s),
-                                                              "title": self.pl.get_title(s)})
-            for du in self.pl.get_dus(s):
-                et.SubElement(current, "filename", {"href": data_dir + du[2:]})
+                                                              "title": self.fcm.get_title(s)})
+            for du in self.fcm.get_dus(s):
+                et.SubElement(current, "filename", {"href": data_dir + du[0]})
         page = et.tostring(root_element, "utf-8")
         of = open(output_dir + filename, "w")
         of.write(subprocess.Popen(["xsltproc", "--nonet", "--novalid", xsl_dir + "page.xsl", "-"],
                                   stdin=subprocess.PIPE, stdout=subprocess.PIPE
                                   ).communicate(page)[0])
         return
-
-
-    def make_index(self, pl):
-        sid_list = pl.get_sids()
-        page = ("<div class='titleblock'>"
-                "<h1>FCOM-NG XHTML Version</h1>"
-                "<h2>Proof of concept</h2>"
-                "</div>"
-                "<div class='index'>")
-        for s in sid_list:
-            if len(s) == 1:
-                page += "<h1>" + pl.get_title(s) + "</h1>"
-                if not pl.get_children(s):
-                    page += ('<p><a href="' +
-                             self.__make_filename__(s) +
-                             '">' + pl.get_title(s) +
-                             '</a></p>')
-            elif len(s) == 2 and pl.get_children(s):
-                page += "<h2>" + pl.get_title(s) + "</h2>"
-            elif len(s)<=3:
-                page += ('<p><a href="' +
-                         self.__make_filename__(s) +
-                         '">' + pl.get_title(s) +
-                         '</a></p>')
-        page += "</div>"
-        of = open(output_dir + "index.html", "w")
-        of.write(html_templates.page % {"title": "Index",
-                                        "fragments": page})
 
 
     def __make_filename__(self, sid):
@@ -179,9 +176,9 @@ class PageFactory:
 
 def main():
     t = et.ElementTree(None, control_file)
-    pl = ProcessList(t.getroot())
-    pf = PageFactory(pl)
-    pf.build_fcom()
+    fcm = FCOMMeta(t.getroot())
+    ff = FCOMFactory(fcm)
+    ff.build_fcom()
 
 
 main()
