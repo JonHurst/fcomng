b8b5de69970214368ae7e81eef7d5d35e59fb737
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2011-11-01 10:45:06 +0000

    Use tabs for selecting alternative fleet

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,55 @@ function showdu(ob) {
 }
 
 
+function change_tab(event) {
+  event = event || window.event;
+  //remove active from all siblings
+  var siblings = this.parentNode.childNodes;
+  for(var i=0; i<siblings.length; i++) {
+    siblings[i].className = siblings[i].className.replace(new RegExp(" active\\b"), "");
+  }
+  //set this node as active
+  this.className += " active";
+  //get id of du to unfold
+  var duid = "duid" + this.href.split("#")[1];
+  console.log(duid);
+  //fold as required
+  var du = this.parentNode.nextSibling;
+  while(du) {
+    if(du.id == duid)
+      du.className = du.className.replace(new RegExp(" folded\\b"), "");
+    else if(du.className.indexOf("folded") == -1) {
+      du.className += " folded";
+    }
+    du = du.nextSibling;
+  }
+  this.blur();
+  return false;
+}
+
+
 function insert_tabs(tabs) {
-  console.log(tabs);
+  var tagdiv = document.createElement("div");
+  tagdiv.className = "tags";
+  var class_string = "main active";
+  var label_length = 80/tabs.length;
+  for(var i=0; i<tabs.length; i++) {
+    var anchor = document.createElement("a");
+    anchor.setAttribute("href", "#" + tabs[i].linkend);
+    anchor.className = class_string;
+    anchor.onclick = change_tab;
+    var label = tabs[i].label.slice(0, label_length);
+    if(label.length < tabs[i].label.length) {
+      label = label.slice(0, -1) + "\u2026";
+    }
+    anchor.textContent = label;
+    tagdiv.appendChild(anchor);
+    class_string = "alternate";
+  }
+  //find du container
+  console.log(tagdiv.innerHTML);
+  var du_container = document.getElementById("duid" + tabs[0].linkend).parentNode;
+  du_container.insertBefore(tagdiv, du_container.firstChild);
 }
 
 
