e9ea262285ea4802ebae2e31db02e87b0bef53db
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2011-11-04 20:54:57 +0000

    Fixes for IE

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,37 @@
+//add indexOf function to IE
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+      "use strict";
+        if (this === void 0 || this === null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 0) {
+            n = Number(arguments[1]);
+            if (n !== n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n !== 0 && n !== Infinity && n !== -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    };
+}
+
 function change_tab(event) {
   event = event || window.event;
   //remove active from all siblings
@@ -38,7 +72,10 @@ function insert_tabs(tabs) {
     if(label.length < tabs[i].label.length) {
       label = label.slice(0, -1) + "\u2026";
     }
-    anchor.textContent = label;
+    if(anchor.textContent != undefined)
+      anchor.textContent = label;
+    else //fix for IE
+      anchor.innerText = label;
     tagdiv.appendChild(anchor);
     class_string = "alternate";
   }
