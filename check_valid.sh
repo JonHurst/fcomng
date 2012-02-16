c424bcb008fc7de3d790a93d21e285c5cb9f0029
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2012-02-16 15:35:09 +0000

    Added check for IMPLWARNINGs to check_valid script

--- check_valid.sh
+++ check_valid.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd /home/jon/proj/fcomng/html
+echo Checking for IMPLWARNINGs
+grep -l IMPLWARNING *
+echo Checking for valid XHTML strict
 xmllint --noout --dtdvalid /usr/share/xml/xhtml/schema/dtd/1.0/xhtml1-strict.dtd --valid *.html
